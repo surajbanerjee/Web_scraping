ac7dec2726e92c1f79412db6197bf89cd2ea2257
Suraj banerjee <58380201+surajbanerjee@users.noreply.github.com>
2023-02-05 19:17:47 +0530

    Code update

--- NFL_Stats_Coding_Exercise_Answer.py
+++ NFL_Stats_Coding_Exercise_Answer.py
@@ -3,29 +3,28 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 #Imports the HTML into python
-url = 'https://www.worldometers.info/world-population/'
-requests.get(url)
+url = 'https://www.nfl.com/standings/league/2019/reg/'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'lxml')
 
 #Subsets the HTML to only get the HTML of our table needed
-table = soup.find('table', class_ = 'table table-striped table-bordered table-hover table-condensed table-list')
-
+table = soup.find('table', {'summary':'Standings - Detailed View'})
+table
 #Gets all the column headers of our table
 headers = []
 for i in table.find_all('th'):
     title = i.text.strip()
     headers.append(title)
-    
+
 #Creates a dataframe using the column headers from our table
 df = pd.DataFrame(columns = headers)
 
 #gets all our data within the table and adds it to our dataframe
-for j in table.find_all('tr')[1:]:
-    row_data = j.find_all('td')
-    row = [tr.text for tr in row_data]
+for row in table.find_all('tr')[1:]:
+    #line below fixes the formatting issue we had with the team names
+    first_td = row.find_all('td')[0].find('div', class_ = 'd3-o-club-fullname').text.strip()
+    data = row.find_all('td')[1:]
+    row_data = [td.text.strip() for td in data]
+    row_data.insert(0,first_td)
     length = len(df)
-    df.loc[length] = row
-    
-#exports the data as a csv
-df.to_csv('A/File/Path/file_name.csv')
+    df.loc[length] = row_data
